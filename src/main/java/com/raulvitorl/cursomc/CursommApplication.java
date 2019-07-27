4679b36b6e5ba7215683177ff5b722c796830b3d
Raul Vitor L <raulawliet@gmail.com>
2019-07-26 22:00:31 -0500

    Projeto Criado

---
+++ src/main/java/com/raulvitorl/cursomc/CursommApplication.java
@@ -0,0 +1,13 @@
+package com.raulvitorl.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursommApplication.class, args);
+	}
+
+}
