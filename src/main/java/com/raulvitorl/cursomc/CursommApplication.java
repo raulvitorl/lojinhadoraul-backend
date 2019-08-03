e45385e950df8a6917f55e393e4422c5f4eabd0c
Raul Vitor L <raulawliet@gmail.com>
2019-08-03 11:01:50 -0500

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/raulvitorl/cursomc/CursommApplication.java
+++ src/main/java/com/raulvitorl/cursomc/CursommApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.raulvitorl.cursomc.domain.Categoria;
 import com.raulvitorl.cursomc.domain.Cidade;
+import com.raulvitorl.cursomc.domain.Cliente;
+import com.raulvitorl.cursomc.domain.Endereco;
 import com.raulvitorl.cursomc.domain.Estado;
 import com.raulvitorl.cursomc.domain.Produto;
+import com.raulvitorl.cursomc.domain.enums.TipoCliente;
 import com.raulvitorl.cursomc.repositories.CategoriaRepository;
 import com.raulvitorl.cursomc.repositories.CidadeRepository;
+import com.raulvitorl.cursomc.repositories.ClienteRepository;
+import com.raulvitorl.cursomc.repositories.EnderecoRepository;
 import com.raulvitorl.cursomc.repositories.EstadoRepository;
 import com.raulvitorl.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursommApplication implements CommandLineRunner{
 	EstadoRepository estadoRepository;
 	@Autowired
 	CidadeRepository cidadeRepository;
+	@Autowired
+	ClienteRepository clienteRepository;
+	@Autowired
+	EnderecoRepository enderecoRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursommApplication.class, args);
 	}
@@ -48,7 +57,14 @@ public class CursommApplication implements CommandLineRunner{
 		Cidade c2 = new Cidade(null,"São Paulo",est2);
 		Cidade c3 = new Cidade(null,"Campinas",est2);
 		
-
+		Cliente cli1 = new 	Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("(68) 9 9245-4310","(68) 9 9959-0932"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","38220834",cli1,c1);
+		Endereco e2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","38777012",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
@@ -63,7 +79,11 @@ public class CursommApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+	
 		
+
 		
 	}
 
