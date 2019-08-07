99b06a8b518f98ec90dca8feea13434b131c72f7
Raul Vitor L <39635395+raulvitorl@users.noreply.github.com>
2019-08-07 16:04:44 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/raulvitorl/cursomc/services/CategoriaService.java
+++ src/main/java/com/raulvitorl/cursomc/services/CategoriaService.java
@@ -25,5 +25,13 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	
 
 }
