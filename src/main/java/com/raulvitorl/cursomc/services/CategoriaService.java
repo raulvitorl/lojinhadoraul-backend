1dcd588276448fda8897470ce8a0b8a413a9f915
Raul Vitor L <39635395+raulvitorl@users.noreply.github.com>
2019-08-07 17:23:03 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/raulvitorl/cursomc/services/CategoriaService.java
+++ src/main/java/com/raulvitorl/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.raulvitorl.cursomc.domain.Categoria;
+import com.raulvitorl.cursomc.dto.CategoriaDTO;
 import com.raulvitorl.cursomc.repositories.CategoriaRepository;
 import com.raulvitorl.cursomc.services.exceptions.DataIntegrityException;
 import com.raulvitorl.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria FromDTO(CategoriaDTO objDto){
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
 
 }
