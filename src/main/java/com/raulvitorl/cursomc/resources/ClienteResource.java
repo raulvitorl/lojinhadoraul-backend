bcb99deaed5fe92819da2edcd4c5b5e3d704c19f
Raul Vitor L <39635395+raulvitorl@users.noreply.github.com>
2019-08-07 12:58:04 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/raulvitorl/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.raulvitorl.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raulvitorl.cursomc.domain.Cliente;
+import com.raulvitorl.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/c	lientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);		
+	}
+	
+}
---
+++ src/main/java/com/raulvitorl/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.raulvitorl.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raulvitorl.cursomc.domain.Cliente;
+import com.raulvitorl.cursomc.repositories.ClienteRepository;
+import com.raulvitorl.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+	
+}
