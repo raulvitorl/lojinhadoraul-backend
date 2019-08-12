5aa7890d29edabba8e241691bd3fed4f981ec245
rvcosta <39635395+raulvitorl@users.noreply.github.com>
2019-08-12 16:28:59 -0300

    Criando o profile de teste

--- src/main/java/com/raulvitorl/cursomc/services/PedidoService.java
+++ src/main/java/com/raulvitorl/cursomc/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.raulvitorl.cursomc.domain.ItemPedido;
 import com.raulvitorl.cursomc.domain.PagamentoComBoleto;
@@ -34,7 +35,7 @@ public class PedidoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 		}
-	
+	@Transactional(readOnly = true)
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
