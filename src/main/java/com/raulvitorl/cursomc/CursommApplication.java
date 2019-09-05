de1050f484e0363eaedaea9c8575933c68e58265
Raul Vitor L <raulawliet@gmail.com>
2019-09-05 09:01:12 -0500

    Salvando primeiro arquivo no S3

--- src/main/java/com/raulvitorl/cursomc/CursommApplication.java
+++ src/main/java/com/raulvitorl/cursomc/CursommApplication.java
@@ -1,12 +1,17 @@
 package com.raulvitorl.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.raulvitorl.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursommApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursommApplication.class, args);
@@ -15,7 +20,7 @@ public class CursommApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-
+		s3Service.uploadFile("C:\\foto.jpg");
 
 	}
 
